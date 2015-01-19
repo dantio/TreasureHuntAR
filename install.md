f715a1dc3288e6b0396c186846038a2d5587d0e3
Eric Wündisch <info@ericwuendisch.de>
2015-01-19 14:04:56 +0100

    add install.md and fix sqlite in server.js

---
+++ install.md
@@ -0,0 +1,25 @@
+#  How to install Node.js Stuff
+
+### Notice the order!
+```
+npm install express
+npm install body-parser
+npm install multer
+npm install sqlite3
+
+```
+
+### start the server just tap in linux
+```
+node server.js
+```
+
+### if the database exists
+```
+rm treasureHuntAR.db
+```
+
+### combine the command with pipe
+```
+rm treasureHuntAR.db | node server.js
+```
--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var pg = require('pg');
-var async = require('async');
-var fs = require('fs');
-var TargetsAPI = require("./TargetsAPI.js");
+var express = require('express'), // REST-App
+    app = express(),
+    bodyParser = require('body-parser'),
+    multer = require('multer')
+    fs = require('fs'),
+    TargetsAPI = require("./TargetsAPI.js"),
+    sqlite3 = require('sqlite3').verbose(),
+    db = new  sqlite3.Database('treasureHuntAR.db');
 
-var CONNECTION_STRING = "postgres://XXX:XXX@XXX.de/dbname";
-var STATUS = {status: false};
-var API_TOKEN = "XXX";
-var API_VERSION = 2;
+var STATUS = {status: false},
+    API_TOKEN = "XXX",
+    API_VERSION = 2;
 
 var API = new TargetsAPI(API_TOKEN, API_VERSION);
+var IMAGE_URL = 'http://ericwuendisch.de/restnode/server/uploads/'; //remember the last /
 
-var IMAGE_URL = 'http://ericwuendisch.de/restnode/uploads/'; //remember the last /
+db.serialize(function () {
+    db.run("CREATE TABLE cache ("
+    + "id INTEGER PRIMARY KEY,"
+    + "description text NOT NULL,"
+    + "picture character varying(512) NOT NULL,"
+    + "latitude double precision NOT NULL,"
+    + "longitude double precision NOT NULL,"
+    + "altitude double precision NOT NULL,"
+    + "target text NULL);");
+});
 
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 app.use(multer()); // for parsing multipart/form-data
 
 
@@ -26,24 +36,25 @@ var server = app.listen(9999, function () {
     console.log('Server is listening at http://%s:%s', host, port)
 });
 
-var computeTargetImage = function (id, picture, callback) {
-    var IMAGE = [IMAGE_URL + picture];
-    API.convert(IMAGE, function (err, url) {
-        if (err) {
-            console.log("Error: " + err);
-        } else {
-            query('UPDATE cache SET target = "' + url + '" WHERE id = ' + id, null, function (result) {
-                if (result.length == 0) {
-                    callback(false);
-                } else {
-                    callback(true);
-                }
-            });
-        }
-    });
-};
-
 app.post('/cache', function (req, res) {
+    var computeTargetImage = function (id, picture, callback) {
+        var IMAGE = [IMAGE_URL + picture];
+        API.convert(IMAGE, function (err, url) {
+            if (err) {
+                console.log("Error: " + err);
+            } else {
+                var q = db.prepare('UPDATE cache SET target = "' + url + '" WHERE id = ' + id);
+                    q.run(function(err){
+                        if (err) {
+                            callback(false);
+                        } else {
+                            callback(true);
+                        }
+                    });
+            }
+        });
+    };
+
     fs.readFile(req.files.thumbnail.path, function (err, data) {
         if (err) {
             res.send(status);
@@ -52,7 +63,7 @@ app.post('/cache', function (req, res) {
             fs.writeFile(newPath, data, function (err) {
 
                 if (err) {
-                    deleteFile(newPath);
+                    fs.unlinkSync(newPath);
                     res.send(STATUS);
                 } else {
 
@@ -62,10 +73,13 @@ app.post('/cache', function (req, res) {
                     var longitude = req.body.longitude.replace(',', '.');
                     var altitude = req.body.altitude.replace(',', '.');
 
-                    query('INSERT INTO cache (description, picture, latitude, longitude, altitude) VALUES (\'' + description + '\',\'' + picture + '\',' + latitude + ',' + longitude + ',' + altitude + ') returning id', newPath, function (result) {
-                        computeTargetImage(result.id, picture, function (state) {
-                            res.send({status: state});
+                    var q = db.prepare('INSERT INTO cache (description, picture, latitude, longitude, altitude) VALUES ("' + description + '","' + picture + '",' + latitude + ',' + longitude + ',' + altitude + ')');
+                    q.run(function(err){
+                        if (err) throw err;
+                        computeTargetImage(this.lastID, picture, function (state) {
+                            res.send({status: true});
                         });
+
                     });
                 }
             });
@@ -74,61 +88,21 @@ app.post('/cache', function (req, res) {
 });
 
 app.get('/cache/:id', function (req, res) {
-    query('SELECT * FROM cache WHERE id = ' + req.params.id, null, function (result) {
-        if (result.length == 0) {
+    db.all('SELECT * FROM cache WHERE id = ' + req.params.id, function(err, rows) {
+        if (rows.length == 0) {
             res.send(STATUS);
         } else {
-            res.send(result);
+            res.send(rows);
         }
     });
 });
 
 app.get('/caches', function (req, res) {
-    query('SELECT * FROM cache', null, function (result) {
-        if (result.length == 0) {
+    db.all('SELECT * FROM cache', function(err, rows) {
+        if (rows.length == 0) {
             res.send(STATUS);
         } else {
-            res.send(result);
+            res.send(rows);
         }
     });
 });
-
-
-var deleteFile = function (filename) {
-    if (filename != null) {
-        fs.unlinkSync(filename);
-    }
-};
-
-var query = function (query, filename, callback) {
-    async.series({
-            result: function (callback) {
-                pg.connect(CONNECTION_STRING, function (err, client, done) {
-                    if (err) {
-                        deleteFile(filename);
-                        return console.error('error fetching client from pool', err);
-                    }
-                    client.query('BEGIN', function (err, result) {
-                        client.query(query, function (err, result) {
-                            done();
-                            if (err) {
-                                client.query('ROLLBACK', function () {
-                                    done();
-                                    deleteFile(filename);
-                                    return console.error('error running query', err);
-                                });
-                            } else {
-                                client.query('COMMIT');
-                                callback(null, result);
-                            }
-                        });
-                    });
-                });
-            }
-        },
-        function (err, results) {
-            console.log(results.result.rows);
-            callback(results.result.rows);
-        });
-};
-
