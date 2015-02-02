0b9d6db8e70d0eb0502a39c19b335d00e5f460e3
Eric Wündisch <info@ericwuendisch.de>
2015-02-02 19:49:28 +0100

    finish post base64 and error output

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ var express = require('express'), // REST-App
     sqlite3 = require('sqlite3').verbose(),
     db = new  sqlite3.Database('treasureHuntAR.db');
 
-var STATUS = {status: false},
-    API_TOKEN = "XXX",
-    API_VERSION = 2;
+var API_TOKEN = "XXX",
+    API_VERSION = 2,
+    LIMIT = 1000000000;
 
 var API = new TargetsAPI(API_TOKEN, API_VERSION);
 var IMAGE_URL = 'http://ericwuendisch.de/restnode/server/uploads/'; //remember the last /
 
-db.serialize(function () {
+/*db.serialize(function () {
     db.run("CREATE TABLE cache ("
     + "id INTEGER PRIMARY KEY,"
     + "description text NOT NULL,"
@@ -23,48 +23,97 @@ db.serialize(function () {
     + "longitude double precision NOT NULL,"
     + "altitude double precision NOT NULL,"
     + "target text NULL);");
-});
+});*/
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-app.use(multer()); // for parsing multipart/form-data
-
+app.use(bodyParser.json({limit: LIMIT})); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true, limit: LIMIT})); // for parsing application/x-www-form-urlencoded
+app.use(multer({limit: LIMIT, size: LIMIT})); // for parsing multipart/form-data
 
 var server = app.listen(9999, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Server is listening at http://%s:%s', host, port)
 });
+var computeTargetImage = function (id, picture, callback) {
+    var IMAGE = [IMAGE_URL + picture];
+    API.convert(IMAGE, function (err, url) {
+        if (err) {
+            console.log("Error: " + err);
+        } else {
+            var q = db.prepare('UPDATE cache SET target = "' + url + '" WHERE id = ' + id);
+            q.run(function(err){
+                if (err) {
+                    callback(false);
+                } else {
+                    callback(true);
+                }
+            });
+        }
+    });
+};
 
-app.post('/cache', function (req, res) {
-    var computeTargetImage = function (id, picture, callback) {
-        var IMAGE = [IMAGE_URL + picture];
-        API.convert(IMAGE, function (err, url) {
-            if (err) {
-                console.log("Error: " + err);
-            } else {
-                var q = db.prepare('UPDATE cache SET target = "' + url + '" WHERE id = ' + id);
-                    q.run(function(err){
-                        if (err) {
-                            callback(false);
-                        } else {
-                            callback(true);
-                        }
-                    });
-            }
-        });
-    };
+var hash = function(len)
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < len; i++ ){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+app.post('/cache64', function (req, res) {
 
     fs.readFile(req.files.thumbnail.path, function (err, data) {
         if (err) {
-            res.send(status);
+            res.send(404).send("Nope");
         } else {
             var newPath = __dirname + "/uploads/" + req.files.thumbnail.originalname;
             fs.writeFile(newPath, data, function (err) {
 
                 if (err) {
                     fs.unlinkSync(newPath);
-                    res.send(STATUS);
+                    res.send(404).send("Nope");
+                } else {
+
+                    var description = req.body.description.toString();
+                    var picture = hash(32).toString();
+                    var latitude = req.body.latitude.replace(',', '.');
+                    var longitude = req.body.longitude.replace(',', '.');
+                    var altitude = req.body.altitude.replace(',', '.');
+
+                    fs.writeFile(__dirname + "/uploads/" +picture+".jpg", new Buffer(req.body.file, "base64"), function(err) {
+                        if (err) throw err;
+
+                        var q = db.prepare('INSERT INTO cache (description, picture, latitude, longitude, altitude) VALUES ("' + description + '","' + picture + '.jpg",' + latitude + ',' + longitude + ',' + altitude + ')');
+                        q.run(function(err){
+                            if (err) throw err;
+                            computeTargetImage(this.lastID, picture, function (state) {
+                                res.send(200).send("Cool");
+                            });
+
+                        });
+                    });
+                }
+            });
+        }
+    });
+});
+
+
+app.post('/cache', function (req, res) {
+
+    fs.readFile(req.files.thumbnail.path, function (err, data) {
+        if (err) {
+            res.send(404).send("Nope");
+        } else {
+            var newPath = __dirname + "/uploads/" + req.files.thumbnail.originalname;
+            fs.writeFile(newPath, data, function (err) {
+
+                if (err) {
+                    fs.unlinkSync(newPath);
+                    res.send(404).send("Nope");
                 } else {
 
                     var description = req.body.description.toString();
@@ -77,7 +126,7 @@ app.post('/cache', function (req, res) {
                     q.run(function(err){
                         if (err) throw err;
                         computeTargetImage(this.lastID, picture, function (state) {
-                            res.send({status: true});
+                            res.send(200).send("Cool");
                         });
 
                     });
@@ -90,7 +139,7 @@ app.post('/cache', function (req, res) {
 app.get('/cache/:id', function (req, res) {
     db.all('SELECT * FROM cache WHERE id = ' + req.params.id, function(err, rows) {
         if (rows.length == 0) {
-            res.send(STATUS);
+            res.send(404).send("Nope");
         } else {
             res.send(rows);
         }
@@ -110,7 +159,7 @@ app.get('/area/:lat/:lon/:radius', function (req, res) {
 
     db.all('SELECT * FROM cache WHERE latitude > '+lat_min+' AND latitude < '+lat_max+' AND longitude > '+lon_min+' AND longitude < '+lon_max, function(err, rows) {
         if (rows.length == 0) {
-            res.send(STATUS);
+            res.send(404).send("Nope");
         } else {
             res.send(rows);
         }
@@ -121,7 +170,7 @@ app.get('/area/:lat/:lon/:radius', function (req, res) {
 app.get('/caches', function (req, res) {
     db.all('SELECT * FROM cache', function(err, rows) {
         if (rows.length == 0) {
-            res.send(STATUS);
+            res.send(404).send("Nope");
         } else {
             res.send(rows);
         }
